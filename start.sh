d2058839e6f264ee44dfc0f5f86f70bfaa42c06d
Tejasvijavagal <160101094+Tejasvijavagal@users.noreply.github.com>
2026-02-07 15:53:16 +0530

    Enable verbose logging and add /health endpoint

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ python initial_data.py
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-echo "Starting Gunicorn..."
-exec gunicorn voting_system.wsgi:application --bind 0.0.0.0:7860 --workers 2 --timeout 120
+echo "Starting Gunicorn with Verbose Logging..."
+exec gunicorn voting_system.wsgi:application --bind 0.0.0.0:7860 --workers 2 --timeout 120 --log-level debug --access-logfile - --error-logfile - --capture-output
